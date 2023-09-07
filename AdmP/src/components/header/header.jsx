c668d55ac0578c371fe140bbf3bf27506dfe086a
Koutsumi <febaccarini@gmail.com>
2023-09-06 23:02:07 -0300

    header criada

--- AdmP/src/components/header/header.jsx
+++ AdmP/src/components/header/header.jsx
@@ -1,12 +1,40 @@
+import { useState } from "react";
+import React from "react";
 
 function Header() {
-    return(
-        <div className="">
-            header
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
 
+  function handleModalState() {
+    modalIsOpen ? setModalIsOpen(false) : setModalIsOpen(true);
+  }
+
+  return (
+    <>
+      <header className="direction-col w-full p-1 bg-principal drop-shadow-lg">
+        <div className="direction-row">
+          <div>
+            <h1 className="text-shadow shadow-black text-[1.2em] text-white uppercase">
+              Projetos
+            </h1>
+          </div>
+          {/* // TODO alterar o "foto" pela tag img e adicionar o src */}
+          <div onClick={() => handleModalState()} className="cursor-pointer">
+            <div className="min-w-[45px] min-h-[45px] bg-white rounded-[100%] direction-col">
+              foto
+            </div>
+          </div>
         </div>
+      </header>
 
-    )
+      <section className={modalIsOpen ? "absolute right-2 p-1 px-2 w-[150px] bg-secundaria drop-shadow-lg rounded" : "hidden"}>
+
+        <a href="" className=""><p>Perfil</p></a>
+        
+        <a href=""><p>Sair</p></a>
+
+      </section>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- AdmP/tailwind.config.cjs
+++ AdmP/tailwind.config.cjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 export default {
   content: [
     "./index.html",
@@ -19,9 +20,23 @@ export default {
       "login": "url('/src/assets/images/gatoLogin.jpg')",
       "none": "url('')",
       
+    },
+    textShadow: {
+      sm: '0 1px 2px var(--tw-shadow-color)',
+      DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+      lg: '0 8px 16px var(--tw-shadow-color)',
     }
   },
-  plugins: [],
+  plugins: [plugin(function ({ matchUtilities, theme }) {
+    matchUtilities(
+      {
+        'text-shadow': (value) => ({
+          textShadow: value,
+        }),
+      },
+      { values: theme('textShadow') }
+    )
+  })],
 }
 
 
